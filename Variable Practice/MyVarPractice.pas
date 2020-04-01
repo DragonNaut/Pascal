ec5314de7a9600803ced21c72fef9a41e6993c02
Matty Ice <33532257+DragonNaut@users.noreply.github.com>
2020-03-31 21:36:50 -0400

    My var Practice

---
+++ Variable Practice/MyVarPractice.pas
@@ -0,0 +1,14 @@
+program MyVarPractice;
+uses crt;
+
+var
+myVar : integer;
+num1 : integer;
+
+
+begin
+        myVar := 123;
+        writeln(myVar);
+        writeln('Press any key to exit');
+        readkey;
+end.
